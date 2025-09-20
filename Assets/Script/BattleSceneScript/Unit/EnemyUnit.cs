ecd391e426a0c61bb41b3600171ec6420b35da53
hayejoon <whiteyejoon@hufs.ac.kr>
2025-09-21 02:15:08 +0900

    battle: 유닛 스프라이트 자동 생성/연결, BattleManager 정리

--- Assets/Script/BattleSceneScript/Unit/EnemyUnit.cs
+++ Assets/Script/BattleSceneScript/Unit/EnemyUnit.cs
@@ -2,20 +2,22 @@ using UnityEngine;
 
 public class EnemyUnit : Unit
 {
-    public override void Init(UnitData Data)
+    public override void Init(UnitData data)
     {
-        UnitData data = Instantiate(Data);
-        Status = new Unit_Status(data);
-        Skill = new Unit_Skill(this);
+        Team = "Enemy";
 
-        name = Status.Name;
-    }
-    public override void TurnStart()
-    {
+        this.Data = Instantiate(data);
 
-    }
-    public override void TurnEnd()
-    {
+        Status = new Unit_Status(this.Data);
+        Skill  = new Unit_Skill(this);
 
+        // 적 유닛은 Front 스프라이트 사용
+        var sr = GetComponent<SpriteRenderer>();
+        if (sr != null && this.Data != null && this.Data.FrontSprite != null)
+            sr.sprite = this.Data.FrontSprite;
     }
+
+    public override void TurnStart() { }
+    public override void TurnEnd() { }
 }
+
