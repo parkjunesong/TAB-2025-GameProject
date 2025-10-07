3b3b488fcc2417f44e53deabfa35aefde94f2c7f
hayejoon <whiteyejoon@hufs.ac.kr>
2025-10-07 12:38:19 +0900

    fixed 인벤토리 UI 구현 및 가방 구현

--- Assets/Script/FieldHoverDetector.cs
+++ Assets/Script/FieldHoverDetector.cs
@@ -2,28 +2,25 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class FieldHoverDetector : MonoBehaviour {
-    [SerializeField] private LayerMask menuZoneMask; 
+    [SerializeField] private LayerMask menuZoneMask;
     [SerializeField] private float hoverDelay = 0.25f;
-    [SerializeField] private UIManager ui;
+    [SerializeField] private UiManager ui;  
 
     private float timer = 0f;
     private bool armed = false;
 
     void Update() {
-        // UI 위면 필드 감지 중단
         if (EventSystem.current != null && EventSystem.current.IsPointerOverGameObject()) {
             ResetHover();
             return;
         }
 
-        // 마우스 월드좌표 (삼항연산자 X)
         Camera cam = Camera.main;
         Vector2 world = Vector2.zero;
         if (cam != null) {
             world = (Vector2)cam.ScreenToWorldPoint(Input.mousePosition);
         }
 
-        // 메뉴 존 위 판정
         bool over = Physics2D.OverlapPoint(world, menuZoneMask);
 
         if (over) {
@@ -37,7 +34,6 @@ public class FieldHoverDetector : MonoBehaviour {
             ui.HideContextMenuIfOpen();
         }
 
-        // ESC로 모두 닫기
         if (Input.GetKeyDown(KeyCode.Escape)) {
             ui.HideContextMenuIfOpen();
             ui.CloseBag();
