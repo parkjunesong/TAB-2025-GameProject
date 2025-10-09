e875a4f3f246555a10c1ae3deb71d88b33138083
hayejoon <whiteyejoon@hufs.ac.kr>
2025-10-09 14:47:40 +0900

    가방 UI 구현중

--- Assets/Script/BattleSceneScript/Data/Itemdata.cs
+++ Assets/Script/BattleSceneScript/Data/Itemdata.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+public enum BagCategory { Tools, Recovery, Pokeballs, TMs }
 
 [CreateAssetMenu(fileName = "ItemData", menuName = "Scriptable Object/ItemData")]
 public class ItemData : ScriptableObject
 {
     public string Name;
     public Sprite Icon;
+    public BagCategory Category;
 }
